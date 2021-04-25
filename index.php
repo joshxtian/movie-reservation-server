1928314f2fa747f053fc04281a93d74b412eaa84
joshxtian <jaysaturno98@gmail.com>
2021-04-25 17:33:12 +0800

    Fixed Links to other page

--- index.php
+++ index.php
@@ -164,7 +164,7 @@ require_once "./php/function.php";
   </footer>
   
   <div class="d-flex justify-content-end fixed-bottom py-5">
-    <a href="/movie-reservation/admin.php" class="btn btn-info">Admin Panel</a>
+    <a href="/admin.php" class="btn btn-info">Admin Panel</a>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
